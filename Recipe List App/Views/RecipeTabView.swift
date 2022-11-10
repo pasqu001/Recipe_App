c92df3601b6f138b9ca4730dbca96fb0916d2092
Kasey Pasqualini <kaseypasqualini@yahoo.com>
2022-11-10 14:41:30 -0500

    -build UI in the FeaturedView

--- Recipe List App/Views/RecipeTabView.swift
+++ Recipe List App/Views/RecipeTabView.swift
@@ -34,6 +34,6 @@ struct RecipeTabView: View {
 
 struct RecipeTabView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeTabView()
+        RecipeTabView().environmentObject(RecipeModel())
     }
 }
